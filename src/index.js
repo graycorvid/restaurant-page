80e81252cc34c87a109ec33da1bb8fbf639ebc3b
Alina Pohl <alinaepohl@gmail.com>
2026-03-03 15:04:53 +0100

    Add event listeners to buttons

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import "./styles.css";
 import { loadPage } from "./home.js";
+import { loadMenu } from "./menu.js";
+
+const homeBtn = document.querySelector(".home");
+const menuBtn = document.querySelector(".menu");
+const aboutBtn = document.querySelector(".about");
 
 loadPage();
+homeBtn.addEventListener("click", loadPage);
+menuBtn.addEventListener("click", loadMenu);
+aboutBtn.addEventListener("click", loadAbout);
